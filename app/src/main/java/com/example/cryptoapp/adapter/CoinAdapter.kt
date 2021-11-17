6f7913e45985f8cd6a85f0f00a915333db53052c
MUradyan <hayk.muradyan@zangi.com>
2021-11-17 22:59:07 +0300

    (HM) is modified something in CoinAdapter.kt

--- app/src/main/java/com/example/cryptoapp/adapter/CoinAdapter.kt
+++ app/src/main/java/com/example/cryptoapp/adapter/CoinAdapter.kt
@@ -2,25 +2,27 @@ package com.example.cryptoapp.adapter
 
 import android.content.Context
 import android.util.TypedValue
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.cryptoapp.R
 import com.example.cryptoapp.data.Coin
 
 class CoinAdapter(val context: Context, var coins: MutableList<Coin>? = null) : RecyclerView.Adapter<CoinViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinViewHolder {
-        val item = TextView(context)
+        val item = LayoutInflater.from(context).inflate(R.layout.item_coin, parent, false)
         return CoinViewHolder(item)
     }
 
     override fun onBindViewHolder(holder: CoinViewHolder, position: Int) {
-        val item = holder.itemView as TextView
-        item.text = coins?.get(position)?.name
-        item.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
-        item.setTextColor(ContextCompat.getColor(context, R.color.black))
+        coins?.get(position)?.let {
+            holder.configureItem(it)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -28,10 +30,22 @@ class CoinAdapter(val context: Context, var coins: MutableList<Coin>? = null) : 
     }
 }
 
-class CoinViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-//    private var textView: TextView? = null
-//
-//    init {
-//        textView = itemView.findViewById(R.id.te)
-//    }
+class CoinViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var nameTextView: TextView? = null
+    var priceTextView: TextView? = null
+    var favoriteImageView: ImageView? = null
+    var iconImageView: ImageView? = null
+
+    init {
+        nameTextView = itemView.findViewById(R.id.name_text_view)
+        priceTextView = itemView.findViewById(R.id.price_text_view)
+        favoriteImageView = itemView.findViewById(R.id.favorite_image_view)
+        iconImageView = itemView.findViewById(R.id.icon_image_view)
+    }
+
+    fun configureItem(coin: Coin) {
+        nameTextView?.text = coin.name
+        priceTextView?.text = coin.price
+        Glide.with(itemView.context).load(coin.icon).into(iconImageView!!)
+    }
 }
--- app/src/main/java/com/example/cryptoapp/data/Coin.kt
+++ app/src/main/java/com/example/cryptoapp/data/Coin.kt
@@ -1,5 +1,9 @@
 package com.example.cryptoapp.data
 
 class Coin {
+    var id = ""
     var name = ""
+    var icon = ""
+    var symbol = ""
+    var price = ""
 }
