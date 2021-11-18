b1a3ccf80b96f6992c2f23949d3171a27ed23756
MUradyan <hayk.muradyan@zangi.com>
2021-11-18 10:28:04 +0300

    (HM) is modified something in CoinRepository.kt

--- app/src/main/java/com/example/cryptoapp/repository/CoinRepository.kt
+++ app/src/main/java/com/example/cryptoapp/repository/CoinRepository.kt
@@ -1,12 +1,30 @@
 package com.example.cryptoapp.repository
 
+import androidx.lifecycle.LiveData
+import com.example.cryptoapp.data.Coin
 import com.example.cryptoapp.data.CoinsResponse
 import com.example.cryptoapp.database.CoinDatabase
 import com.example.cryptoapp.network.RetrofitInstance
 import retrofit2.Response
 
-class CoinRepository(db: CoinDatabase) {
+class CoinRepository(val db: CoinDatabase) {
     suspend fun getAllCoins(): Response<CoinsResponse> {
         return RetrofitInstance.api.getCoins()
+    }
+
+    fun getSavedCoins(): LiveData<Coin> {
+        return db.getCoinDao().getSavedCoins()
+    }
+
+    fun updateCoin(coin: Coin) {
+        db.getCoinDao().update(coin)
+    }
+
+    fun insertCoin(coin: Coin) {
+        db.getCoinDao().insert(coin)
+    }
+
+    fun deleteCoin(coin: Coin) {
+        db.getCoinDao().delete(coin)
     }
 }
